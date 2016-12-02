aaaa84828466c7717f389295a5d24d07e68efab8
Chuan Ren <renkelvin@gmail.com>
2016-12-01 18:09:08 -0800

    ~

--- Willer/DeckViewController.swift
+++ Willer/DeckViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DeckViewController: UIViewController {
+class DeckViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +21,29 @@ class DeckViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Table view data source
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DeckTableViewCellReuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    // MARK: - Table view delegate
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
     /*
     // MARK: - Navigation
